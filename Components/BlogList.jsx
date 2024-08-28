f83ec02818c0a241c81c3609b24a49d9ae85ed94
NoobMahbub <Mahbubbengali@gmail.com>
2024-08-28 20:30:14 +0600

    fixed the bug

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -15,7 +15,7 @@ export default function BlogList() {
             </div>
             <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24'>
                 {
-                    blog_data.filter((item) => item.category === "All" ? true : item.category === menu).map((item, index) => <Blogcard image={item.image}
+                    blog_data.filter((item) => menu === "All" ? true : item.category === menu).map((item, index) => <Blogcard image={item.image}
                         title={item.title}
                         description={item.description}
                         category={item.category}
