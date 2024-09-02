654e36549899d6b3f462cc3968a33d91134bb5eb
NoobMahbub <Mahbubbengali@gmail.com>
2024-09-02 22:53:45 +0600

    hidden th navbar from admin dashboard

--- Components/shared/Navbar.jsx
+++ Components/shared/Navbar.jsx
@@ -3,16 +3,17 @@ import { assets } from "@/Assets/assets"
 import { willNotRender } from "@/utils/willNotRender"
 import Image from "next/image"
 import Link from "next/link"
-import { usePathname } from "next/navigation"
+import { usePathname, useRouter } from "next/navigation"
 import React, { useState } from "react"
 
 export default function Navbar() {
   const [isToggleOpen, setIsToggleOpen] = useState(false)
-  const path = usePathname()
+  const pathname = usePathname()
+  const isAdminRoute = pathname.startsWith('/admin')
   return (
     <>
       {/*<!-- Component: Navbar with CTA --> */}
-      {!willNotRender.includes(path) && (
+      {!isAdminRoute && (
         <header className="border-b-1 relative z-20 w-full border-b border-slate-200 bg-white/90 shadow-lg shadow-slate-700/5 after:absolute after:left-0 after:top-full after:z-10 after:block after:h-px after:w-full after:bg-slate-200 lg:border-slate-200 lg:backdrop-blur-sm lg:after:hidden">
           <div className="relative mx-auto max-w-full px-6 lg:max-w-5xl xl:max-w-7xl 2xl:max-w-[96rem]">
             <nav
