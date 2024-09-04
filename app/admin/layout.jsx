6c88b2b738456f9491d49261af4160ffcc9352e3
NoobMahbub <Mahbubbengali@gmail.com>
2024-09-04 11:10:16 +0600

    added toast

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,10 +1,13 @@
 import Sidebar from '@/Components/AdminComponents/Sidebar'
 import React from 'react'
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 export default function Layout({ children }) {
     return (
         <>
+
             <div>
+                <ToastContainer theme='dark' />
                 <Sidebar />
             </div>
             {children}
