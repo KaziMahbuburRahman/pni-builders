0ad7596070185c1a34fc63f776c65657b791c612
NoobMahbub <Mahbubbengali@gmail.com>
2024-09-07 00:00:28 +0600

    created blog list feature for admin dashboard

--- app/admin/blogList/page.jsx
+++ app/admin/blogList/page.jsx
@@ -1,34 +1,47 @@
 "use client"
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 
 function Index() {
     const [show, setShow] = useState(null);
+    const [blogs, setBlogs] = useState([])
+
+    const fetchBlogs = async () => {
+        const response = await axios.get('/api/blog')
+        setBlogs(response.data.blogs)
+        console.log(response.data.blogs)
+    }
+    useEffect(()=>{
+        fetchBlogs();
+    },[])
     return (
         <>
             
-    <div className="overflow-x-auto container editor mx-auto w-10/12 flex flex-col text-gray-800 max-w-4xl">
-    <table className="min-w-[90%] shadow-md border mx-auto border-gray-100 m-5">
+    <div className="overflow-x-auto container editor mx-auto flex flex-col text-gray-800 max-w-4xl">
+    <table className="lg:min-w-[95%] max-w-[95%] shadow-md border mx-auto border-gray-100 m-5">
         <thead>
             <tr className="bg-[#0095FF] text-white">
-                <th className="py-4 px-6 text-lg text-left border-b">Product Image</th>
-                <th className="py-4 px-6 text-lg text-left border-b">Product Name</th>
-                <th className="py-4 px-6 text-lg text-left border-b">Price</th>
+                <th className="py-4 px-6 text-lg text-left border-b">Blog Title</th>
+                <th className="py-4 px-6 text-lg text-left border-b">Author</th>
                 <th className="py-4 px-6 text-lg border-b text-end">Action</th>
             </tr>
         </thead>
         <tbody>
-            <tr className="hover:bg-gray-50 border-b transition duration-300">
-                <td className="py-4 px-4 flex justify-start">
-                    <img src="https://source.unsplash.com/64x64/?microphone" alt="table navigate ui" className="h-16 w-16 object-cover bg-gray-300" />
-                </td>
-                <td className="py-4 px-6 border-b text-xl font-medium">Dual Speaker</td>
-                <td className="py-4 px-6 border-b text-lg font-medium">$99.99</td>
+            
+           {
+            
+            blogs.map(blog=> (<tr className="hover:bg-gray-50 border-b transition duration-300">
+               
+                <td className="py-4 px-6 border-b text-xl font-medium">{blog.title}</td>
+                <td className="py-4 px-6 border-b text-lg font-medium">{blog.author}</td>
                 <td className="py-4 px-6 border-b text-end">
                     <button className="bg-blue-500 hover:scale-110 scale-100 transition-all duration-100 text-white py-2 px-4 rounded-md">Details</button>
                 </td>
-            </tr>
-            <tr className="hover:bg-gray-50 border-b transition duration-300">
+            </tr>)
+            )
+           }
+            {/* <tr className="hover:bg-gray-50 border-b transition duration-300">
                 <td className="py-4 px-4 flex justify-start">
                     <img src="https://source.unsplash.com/64x64/?phone" alt="table navigate ui" className="h-16 w-16 object-cover bg-gray-300" />
                 </td>
@@ -37,7 +50,8 @@ function Index() {
                 <td className="py-4 px-6 border-b text-end">
                     <button className="bg-blue-500 hover:scale-110 scale-100 transition-all duration-100 text-white py-2 px-4 rounded-md">Details</button>
                 </td>
-            </tr>
+            </tr> */}
+            
         </tbody>
     </table>
 </div>
