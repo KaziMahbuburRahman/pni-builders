c030e3895b53db88add5f447a863191c3d6e8d97
NoobMahbub <Mahbubbengali@gmail.com>
2024-09-07 00:19:48 +0600

    fixed sidebar button style

--- Components/AdminComponents/Sidebar.jsx
+++ Components/AdminComponents/Sidebar.jsx
@@ -11,7 +11,7 @@ export default function Sidebar() {
             <button
                 title="Side navigation"
                 type="button"
-                className={`visible fixed left-6 top-6 z-40 order-10 block h-10 w-10 self-center rounded bg-white opacity-100 lg:hidden ${isSideNavOpen
+                className={`visible fixed left-2 top-6 z-40 order-10 block h-10 w-10 self-center rounded bg-white opacity-100 lg:hidden ${isSideNavOpen
                     ? "visible opacity-100 [&_span:nth-child(1)]:w-6 [&_span:nth-child(1)]:translate-y-0 [&_span:nth-child(1)]:rotate-45 [&_span:nth-child(3)]:w-0 [&_span:nth-child(2)]:-rotate-45 "
                     : ""
                     }`}
